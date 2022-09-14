2ffd58a4207bce455fc2390a0081f2d8acca0924
Miguel C <migs.castaneda@gmail.com>
2022-09-14 19:43:12 -0400

    rattata seed

---
+++ seeders/Starting-Pokemon.js
@@ -0,0 +1,9 @@
+const db = require('../models')
+
+async function seed() {
+    name: "Rattata";
+    type: "Normal";
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png"
+}
+
+seed()
